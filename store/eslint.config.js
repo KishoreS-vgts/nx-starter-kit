f5b3630f0a39fa78871662f7bc838fc4087901f2
KishoreS-vgts <kishore.s@vishgyana.com>
2024-10-22 12:35:17 +0530

    fix: removed dups

--- store/eslint.config.js
+++ store/eslint.config.js
@@ -1,4 +1,2 @@
 const baseConfig = require('../eslint.base.config.js')
-const baseConfig = require('../eslint.config.js')
-
-module.exports = [...baseConfig, ...baseConfig]
+module.exports = [...baseConfig]
