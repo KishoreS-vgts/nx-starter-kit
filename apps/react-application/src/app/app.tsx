fc4bb826ce25bf71bebbf733b6671ae9b2a3c790
KishoreS-vgts <kishore.s@vishgyana.com>
2024-10-23 16:39:22 +0530

    fix: updated code to deploy

--- apps/react-application/src/app/app.tsx
+++ apps/react-application/src/app/app.tsx
@@ -2,7 +2,7 @@ export function App() {
   return (
     <div className="flex flex-col justify-center items-center h-screen text-2xl">
       <div className="space-y-2">
-        <h1 className="text-base underline">Login!</h1>
+        <h1 className="text-base underline">Login hi</h1>
         {/* <Card></Card> */}
       </div>
     </div>
--- apps/react-application/vite.config.ts
+++ apps/react-application/vite.config.ts
@@ -1,15 +1,15 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin'
 
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/react-application',
   server: {
-    port: 4200,
-    host: 'localhost',
+    port: 3001,
+    host: '0.0.0.0',
   },
   preview: {
     port: 4300,
@@ -22,11 +22,11 @@ export default defineConfig({
   // },
   build: {
     // outDir: '../../dist/apps/react-application',
-    outDir : "./dist/react-application",
+    outDir: './dist',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
     },
   },
-});
+})
