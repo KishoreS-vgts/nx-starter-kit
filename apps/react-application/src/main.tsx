a54a0c6520f0ca9cd5d563b237f99bd43961a923
KishoreS-vgts <kishore.s@vishgyana.com>
2024-10-24 14:50:17 +0530

    fix: removed all the routing logic from main.ts

--- apps/react-application/src/main.tsx
+++ apps/react-application/src/main.tsx
@@ -1,25 +1,10 @@
 import { StrictMode } from 'react'
 import * as ReactDOM from 'react-dom/client'
-
-import App from './app/app'
-
-import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-
-  {
-    path: '/dashboard',
-    element: <App />,
-  },
-])
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 )
