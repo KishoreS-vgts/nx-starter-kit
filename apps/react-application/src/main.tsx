1a32f9af4919a70cc27bea88838ad2f195c7f958
KishoreS-vgts <kishore.s@vishgyana.com>
2024-11-01 18:32:06 +0530

    feat: query provider added

--- apps/react-application/src/main.tsx
+++ apps/react-application/src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 
 import * as ReactDOM from 'react-dom/client'
 
+import { ReactQueryProvider } from '@react-monorepo/core-provider'
+
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <StrictMode>
-    <App />
+    <ReactQueryProvider>
+      <App />
+    </ReactQueryProvider>
   </StrictMode>
 )
