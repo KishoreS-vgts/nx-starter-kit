f63efa10301c471b61c8953809bc46598f3eaef0
KishoreS-vgts <kishore.s@vishgyana.com>
2024-11-04 07:31:30 +0530

    fix: updated route and routeConfig

--- apps/react-application/src/routeConfig.tsx
+++ apps/react-application/src/routeConfig.tsx
@@ -1,41 +1,43 @@
 import { lazy } from 'react'
 
-import { Navigate } from 'react-router-dom'
-
 import { Route } from '@react-monorepo/core-types'
 
+import Layout from './Layout'
 import NotFound from './pages/404.page'
 
 const Login = lazy(() => import('./pages/Login.page'))
 const DashBoard = lazy(() => import('./pages/DashBoard.page'))
-
+const User = lazy(() => import('./pages/User.page'))
 // permissions can be sent from the backend so that we can maintain a secure routing
 
 export const appRoutes: Route[] = [
   {
     path: '/login',
-    element: <Login />,
-    roles: ['user', 'admin'],
-  },
-  {
-    path: '/',
-    element: <Navigate to="/login" />,
+    element: Login,
     roles: ['user', 'admin'],
   },
   {
     path: '*',
-    element: <NotFound />,
+    element: NotFound,
     roles: ['user', 'admin'],
   },
   {
     path: '/dashboard',
-    element: <DashBoard />,
+    element: DashBoard,
     roles: ['admin'],
     protected: true,
+    layout: Layout,
+  },
+  {
+    path: '/admin/profile',
+    element: User,
+    roles: ['admin'],
+    protected: true,
+    layout: Layout,
   },
   {
     path: '/user/dashboard',
-    element: <div>Hello user welcome</div>,
+    element: User,
     roles: ['user'],
     protected: true,
   },
--- core-type/types/routes.ts
+++ core-type/types/routes.ts
@@ -1,8 +1,8 @@
 export interface Route {
   path: string
-  element: React.ReactElement
+  element: React.ComponentType
   roles: string[]
-  layout?: JSX.Element
+  layout?: React.ComponentType<{ children: React.ReactNode }>
   children?: Route[]
   // this flag we can use to protect the most critical pages.
   //Like a second layer on top of the initial filtering layer
