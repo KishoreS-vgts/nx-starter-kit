b3a1488836f172df7462dd980f2af9f6ab46bb26
KishoreS-vgts <kishore.s@vishgyana.com>
2024-11-04 07:31:54 +0530

    fix: new pages added

--- apps/react-application/src/pages/DashBoard.page.tsx
+++ apps/react-application/src/pages/DashBoard.page.tsx
@@ -1,7 +1,17 @@
+import { Trans, useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
+
 export default function DashBoard() {
+  const { t } = useTranslation()
   return (
-    <div className="flex flex-col justify-center items-center h-screen text-2xl">
-      Welcome to the NX monorepo
+    <div className="flex flex-col justify-center items-center h-full text-2xl">
+      {t('Welcome to React')}
+      <Trans i18nKey="adminDashboard">
+        <p>
+          This is <strong>admin's</strong> dashboard
+        </p>
+      </Trans>
+      <Link to="/admin/profile">Go to profile</Link>
     </div>
   )
 }
